aec9c5324cdbd15ef0dd4d94cbba4434117424a7
longfei.fu <fulongfei@bgi.com>
2026-05-11 15:04:04 +0800

    update

--- bin/cmd.sh
+++
@@ -1,2 +0,0 @@
-perl WhatsHap_trio_phasing.pl $PWD/gene_bam/JX_2/FHOD3_25B09089387.bam $PWD/gene_bam/JX_2/FHOD3_25B09089386.bam $PWD/gene_bam/JX_2/FHOD3_25B09089402.bam $PWD/pedigree_Hap
-perl WhatsHap_trio_phasing.pl $PWD/gene_bam/JX_5/FHOD3_25B09089394.bam $PWD/gene_bam/JX_5/FHOD3_25B09089395.bam $PWD/gene_bam/JX_5/FHOD3_25B09089393.bam $PWD/pedigree_Hap
